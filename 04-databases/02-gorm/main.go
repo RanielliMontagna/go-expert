e61cab0ecf2f4bc394ab78a919e6b72ca14bd599
Ranielli Montagna <raniellimontagna@hotmail.com>
2024-04-17 08:43:09 -0300

    feat(database): :sparkles: basic operations with gorm

--- 04-databases/02-gorm/main.go
+++ 04-databases/02-gorm/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -29,4 +31,40 @@ func main() {
 	}
 
 	db.Create(&products)
+
+	// Select one
+	var product Product
+	db.First(&product, 1)
+	db.First(&product, "name = ?", "Mouse")
+	fmt.Println(product)
+
+	// Select all
+	db.Find(&products)
+	for _, p := range products {
+		fmt.Println(p)
+	}
+
+	// db.Limit(2).Offset(2).Find(&products)
+	// for _, p := range products {
+	// 	fmt.Println(p)
+	// }
+
+	// Where
+	db.Where("price > ?", 50).Find(&products)
+	for _, p := range products {
+		fmt.Println(p)
+	}
+
+	db.Where("name LIKE ?", "%o%").Find(&products)
+	for _, p := range products {
+		fmt.Println(p)
+	}
+
+	// Update and Delete
+	var productToDelete Product
+	db.First(&productToDelete, 1)
+	productToDelete.Name = "Deleted Product"
+	db.Save(&productToDelete)
+
+	db.Delete(&productToDelete)
 }
