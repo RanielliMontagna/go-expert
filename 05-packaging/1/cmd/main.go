66920fd330bb4bd2100bec20fbeac63075ff8c56
Ranielli Montagna <raniellimontagna@hotmail.com>
2024-04-19 08:41:05 -0300

    feat: :sparkles: go mod replace

--- 05-packaging/1/cmd/main.go
+++ 05-packaging/1/cmd/main.go
@@ -9,6 +9,7 @@ func main() {
 	fmt.Println("Hello, World!")
 
 	// Create a new Math struct and call the Add method
-	m := math.Math{A: 1, B: 2}
+	// m := math.Math{}
+	m := math.NewMath(1, 2)
 	fmt.Println(m.Add())
 }
---
+++ 05-packaging/3/system/go.mod
@@ -0,0 +1,7 @@
+module goexpert/packaging/3/system
+
+go 1.22.2
+
+replace goexpert/packaging/3/math => ../math
+
+require goexpert/packaging/3/math v0.0.0-00010101000000-000000000000
