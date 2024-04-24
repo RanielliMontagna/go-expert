7fd4ef8830cd263166ae4d4701673baa4432728c
Ranielli Montagna <raniellimontagna@hotmail.com>
2024-04-24 08:41:39 -0300

    test: :white_check_mark: testing user db

--- 06-apis/internal/infra/database/user_db_test.go
+++ 06-apis/internal/infra/database/user_db_test.go
@@ -12,9 +12,7 @@ import (
 
 func TestUserDB(t *testing.T) {
 	t.Run("create new user", func(t *testing.T) {
-
-		db, err := gorm.Open(sqlite.Open("file::memory"), &gorm.Config{})
-
+		db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
 		if err != nil {
 			t.Error(err)
 		}
@@ -29,6 +27,28 @@ func TestUserDB(t *testing.T) {
 
 		var userFound entity.User
 		err = db.First(&userFound, "id = ?", user.ID).Error
+		assert.Nil(t, err)
+		assert.Equal(t, user.ID, userFound.ID)
+		assert.Equal(t, user.Name, userFound.Name)
+		assert.Equal(t, user.Email, userFound.Email)
+		assert.NotNil(t, userFound.Password)
+	})
+
+	t.Run("find user by email", func(t *testing.T) {
+		db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+		if err != nil {
+			t.Error(err)
+		}
+
+		db.AutoMigrate(entity.User{})
+
+		user, _ := entity.NewUser("John Doe", "john@doe.com", "password")
+		userDB := NewUser(db)
+
+		err = userDB.Create(user)
+		assert.Nil(t, err)
+
+		userFound, err := userDB.FindByEmail(user.Email)
 		assert.Nil(t, err)
 		assert.Equal(t, user.ID, userFound.ID)
 		assert.Equal(t, user.Name, userFound.Name)
