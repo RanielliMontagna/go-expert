2464509e06f1fb16213521ee7d385aa649dd7b69
Ranielli Montagna <raniellimontagna@hotmail.com>
2024-05-06 08:26:57 -0300

    feat: :sparkles: atomic operations

--- 07-multithreading/03-concurrency-problem/main.go
+++ 07-multithreading/03-concurrency-problem/main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
 var number uint64 = 0
 
 func main() {
+	// m := sync.Mutex{}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		number++
+		// m.Lock()
+		atomic.AddUint64(&number, 1)
+		// number++
+		// m.Unlock()
 		time.Sleep(300 * time.Millisecond)
 		w.Write([]byte(fmt.Sprintf("You are visitor number %d", number)))
 	})
